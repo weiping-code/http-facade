b777463e861268f6db4cd031d2e6f957c3c54e6a
weiping-code <hustwaitone@163.com>
2025-04-08 22:16:49 +0800

    feat: add aliyun appcode auth filter
    Signed-off-by: weiping-code <hustwaitone@163.com>

---
+++ http-facade-tests/src/test/java/io/github/openfacade/http/tests/HttpClientAliyunAppCodeAuthFilterTest.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2024 OpenFacade Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.openfacade.http.tests;
+
+import io.github.openfacade.http.AliyunAppCodeAuthRequestFilter;
+import io.github.openfacade.http.HttpClient;
+import io.github.openfacade.http.HttpClientConfig;
+import io.github.openfacade.http.HttpClientFactory;
+import io.github.openfacade.http.HttpMethod;
+import io.github.openfacade.http.HttpRequest;
+import io.github.openfacade.http.HttpResponse;
+import io.github.openfacade.http.HttpServer;
+import io.github.openfacade.http.HttpServerConfig;
+import io.github.openfacade.http.HttpServerFactory;
+import io.github.openfacade.http.RequestFilter;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+
+@Slf4j
+public class HttpClientAliyunAppCodeAuthFilterTest extends BaseTest {
+    @ParameterizedTest
+    @MethodSource("clientServerConfigProvider")
+    void testHttpClientServerCombinations(HttpClientConfig clientConfig, HttpServerConfig serverConfig) throws Exception {
+        ArrayList<RequestFilter> requestFilters = new ArrayList<>();
+        requestFilters.add(new AliyunAppCodeAuthRequestFilter("xxx"));
+        clientConfig.setRequestFilters(requestFilters);
+        log.info("client engine {}, server engine {}", clientConfig.engine(), serverConfig.engine());
+        HttpClient client = HttpClientFactory.createHttpClient(clientConfig);
+        HttpServer server = HttpServerFactory.createHttpServer(serverConfig);
+
+        HttpMethod method = HttpMethod.GET;
+        server.addRoute("/hello", method, request -> {
+            Map<String, List<String>> headers = request.headers();
+            List<String> authorization = headers.get("Authorization");
+            if (authorization == null) {
+                // tomcat will convert the header name to lower case
+                authorization = headers.get("authorization");
+            }
+            if (authorization == null || !"APPCODE xxx".equals(authorization.get(0))) {
+                return CompletableFuture.completedFuture(new HttpResponse(401, new byte[0]));
+            }
+            HttpResponse response = new HttpResponse(200, String.format("%s method called!", method).getBytes());
+            return CompletableFuture.completedFuture(response);
+        });
+
+        server.start().join();
+
+        String url = String.format("http://localhost:%d/hello", server.listenPort());
+
+        HttpRequest request = new HttpRequest(url, method);
+        log.info("sending {} request to url: {}", method, url);
+        HttpResponse response = client.sendSync(request);
+
+        Assertions.assertEquals(200, response.statusCode());
+        Assertions.assertNotNull(response.body());
+        Assertions.assertEquals(String.format("%s method called!", method), new String(response.body()));
+
+        client.close();
+        server.stop().join();
+    }
+}
